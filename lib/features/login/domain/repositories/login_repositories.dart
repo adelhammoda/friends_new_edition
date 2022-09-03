babfecaa2244541a2b33f4d0face47c7857324c8
adelhammoda <adelhammoda6144@gmail.com>
2022-09-03 15:17:16 +0300

    FP-19 cash email and password use cases and fix import in login

--- lib/features/login/domain/repositories/login_repositories.dart
+++ lib/features/login/domain/repositories/login_repositories.dart
@@ -4,7 +4,6 @@ import 'package:flutter/widg'
     'ets.dart';
 
 import '../../../../core/exception/failure.dart';
-import '../entities/user_entity.dart';
 
 
 abstract class LoginRepositories {
@@ -12,24 +11,26 @@ abstract class LoginRepositories {
   ///firebase auth .
   ///return [UserCredential] if the process completed successfully
   ///and return [Failure[ when error happened.
-  Future<Either<Failure,UserCredential>> loginWithFacebook();
+  Future<Either<Failure, UserCredential>> loginWithFacebook();
+
   ///login with google account that supported by
   ///firebase auth .
   ///return [UserCredential] if the process completed successfully
   ///and return [Failure[ when error happened.
-  Future<Either<Failure,UserCredential>> loginWithApple ();
+  Future<Either<Failure, UserCredential>> loginWithApple();
+
   ///login with google account that supported by
   ///firebase auth .
   ///return [UserCredential] if the process completed successfully
   ///and return [Failure[ when error happened.
-  Future<Either<Failure,UserCredential>> loginWithGoogle();
+  Future<Either<Failure, UserCredential>> loginWithGoogle();
+
   ///login with email and password using firebase  packages
   ///to login.
   ///return [UserCredential] when completed successfully
   ///and return Type of [Failure] when error happened
   Future<Either<Failure, UserCredential>> loginWithEmailAndPassword({
     required String email,
-
     required String password,
   });
 
@@ -37,17 +38,21 @@ abstract class LoginRepositories {
   ///return no thing if the navigation completed successfully
   ///where it return  type of [Failure] when any exception happened.
   Either<Failure, Null> forgetPassword(BuildContext context);
+
   ///navigate to create account page it's hold
   ///the email and password and pass them to {email controller}
   ///and {password controller}
   ///Return a [Failure]  when any navigation error happened
   Either<Failure, Null> createAccount({
-  required String email,
+    required String email,
     required String password,
-});
+  });
 
   ///we need to cash user after he logged in successfully to
   ///auto log in in next time  he enter the app
   ///Return [Failure] when any cashing exception happened
-  Future<Either<Failure,User>> cashUser(UserEntity user);
+  Future<Either<Failure, Null>> cashEmailAndPassword({
+    required String email,
+    required String password,
+  });
 }
