250ec530b2172fe8027b912a36870aa038e71af7
adelhammoda <adelhammoda6144@gmail.com>
2022-09-03 14:47:01 +0300

    FP-19 login with apple use cases

---
+++ lib/features/login/domain/use_cases/login_with_apple_usecase.dart
@@ -0,0 +1,22 @@
+
+
+
+
+
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:friends/features/login/domain/repositories/login_repositories.dart';
+
+import '../../../../core/exception/failure.dart';
+
+class LoginWithApple{
+
+
+
+  final LoginRepositories repositories;
+
+  LoginWithApple(this.repositories);
+
+  Future<Either<Failure,UserCredential>> call()async=>await repositories.loginWithApple();
+
+}
